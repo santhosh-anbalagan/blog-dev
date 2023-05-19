5b0ad4123339cb0b8265cf66f73de54a1700af74
santhosh-anbalagan <46365378+santhosh-anbalagan@users.noreply.github.com>
2023-05-19 21:01:26 +0530

    fixes

--- apps/enji.dev/src/contents/index/CleanIntuitive.tsx
+++ apps/enji.dev/src/contents/index/CleanIntuitive.tsx
@@ -5,7 +5,9 @@ import { SectionButton } from '@/components/sections/SectionButton';
 import SectionContent from '@/components/sections/SectionContent';
 import SectionTitle from '@/components/sections/SectionTitle';
 
-import type { TodoItemState } from '@/contents/index/Cards/TodoItemDT';
+import TodoItem from '@/contents/index/Cards/TodoItem';
+
+import type { TodoItemState } from '@/contents/index/Cards/TodoItem';
 
 type Content = {
   state: TodoItemState;
@@ -36,7 +38,7 @@ const content: Array<Content> = [
 ];
 
 function CleanIntuitive() {
-  const [currentState, setCurrentState] = useState<Content | null>(content[0]);
+  const [currentState, setCurrentState] = useState<Content | null>(null);
 
   return (
     <>
@@ -47,6 +49,55 @@ function CleanIntuitive() {
           description="I’m excited to explore the latest tech trends and get hands-on experience with cutting-edge technology."
         />
       </header>
+      <SectionContent>
+        <div className={clsx('flex', 'lg:gap-12')}>
+          <div
+            className={clsx('-mt-8 hidden flex-1 flex-col gap-3', 'lg:flex')}
+          >
+            {content.map((item, i) => (
+              <SectionButton
+                key={item.state}
+                title={item.title}
+                description={item.description}
+                icon={i + 1}
+                active={currentState?.state === item.state}
+                onClick={() => setCurrentState(item)}
+              />
+            ))}
+          </div>
+          <div
+            className={clsx('relative flex flex-1 items-center justify-center')}
+          >
+            <div
+              className={clsx('-mt-8 flex gap-4', 'md:gap-6 lg:top-8 lg:mt-0')}
+            >
+              <div>
+                <TodoItem
+                  state={
+                    currentState
+                      ? currentState.shows
+                      : ['iot', 'digitaltwin', 'augment', 'effects']
+                  }
+                />
+              </div>
+              <div className={clsx('hidden', 'sm:block lg:hidden')}>
+                <TodoItem
+                  state={
+                    currentState
+                      ? currentState.shows
+                      : ['iot', 'digitaltwin', 'augment', 'effects']
+                  }
+                  title="UI Implementation"
+                  description="Start creating UI components using React and Tailwind CSS."
+                  date="10:00 AM · Tomorrow"
+                  tag1="Design"
+                  tag2="Components"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </SectionContent>
     </>
   );
 }
